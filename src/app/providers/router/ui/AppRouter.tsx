e491d2ef4e6f1e420869a727fc9d730a6b0ad39a
vizachem <vizachem@gmail.com>
2023-05-12 18:25:09 +0300

    fix bags

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -10,6 +10,7 @@ const AppRouter = () => {
                     <Route
                         key={path}
                         path={path}
+                        element={element}
                     />
                 ))}
             </Routes>
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -15,11 +15,11 @@ export const RoutePath: Record<AppRoutes, string> = {
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
-        path: RoutePath.main,
-        element: <MainPage/>
+        path:RoutePath.main,
+        element: <MainPage />
     },
     [AppRoutes.ABOUT]: {
-        path: RoutePath.about,
-        element: <AboutPage/>
+        path:RoutePath.about,
+        element: <AboutPage />
     }
 }
